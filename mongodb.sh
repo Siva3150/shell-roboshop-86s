3748e93a4a8c4666ca9462a6a531cea697e89344
Siva prasad <sivaprasad3150@gmail.com>
2026-02-06 17:53:48 +0530

    shell-script

--- mongodb.sh
+++ mongodb.sh
@@ -30,21 +30,21 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
 
 }
 
-cp mongodb.repo /etc/yum.repos.d/mongo.repo
+cp mongodb.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
 VALIDATE $? "Adding mongodb repo" 
 
-dnf install mongodb-org -y
+dnf install mongodb-org -y &>>$LOG_FILE
 VALIDATE $? "Installing mongodb" 
 
-systemctl enable mongod 
+systemctl enable mongod  &>>$LOG_FILE
 VALIDATE $? "Enabling mongodb"
 
-systemctl start mongod 
+systemctl start mongod &>>$LOG_FILE
 VALIDATE $? "Starting mongodb"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongodb.conf
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongodb.conf &>>$LOG_FILE
 VALIDATE $? "Allowing remote connections to mongodb"
 
-systemctl restart mongod
+systemctl restart mongod &>>$LOG_FILE
 VALIDATE $? "Restarting mongodb"
 
