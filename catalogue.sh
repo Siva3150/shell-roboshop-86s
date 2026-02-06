9f29775b5c5aeddec705231424570630fbc5d1c3
Siva prasad <sivaprasad3150@gmail.com>
2026-02-06 18:59:11 +0530

    shell-script

--- catalogue.sh
+++ catalogue.sh
@@ -87,7 +87,7 @@ VALIDATE $? "Copying mongo repo"
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing mongodb client"
 
-INDEX=$(mongosh $MONGODB_HOST --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+INDEX=$(mongosh $MONGODB_HOST --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')") &>>$LOG_FILE
 if [ $INDEX -le 0 ]; then 
    mongosh --host $MONGODB_HOST </app/db/master-data.js 
    VALIDATE $? "Loading schema"
